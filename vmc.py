b29647a537d0b6ec467787fbcd82c364e82c9b83
Alessandro Cerati <alessandro.cerati@studio.unibo.it>
2026-02-04 23:47:10 +0100

    Updated stepping to better implement Metropolis algo

--- vmc.py
+++ vmc.py
@@ -1,14 +1,13 @@
 import numpy as np
 
-class config_walker:
+class ConfigWalker:
   """
   Random walker representing the configuration space position of a system of N particles in d dimensions of size L
   """
   rng_seed = 1
   rng = np.random.default_rng(rng_seed)
-  next_id = 0
 
-  def __init__(self, particles = 2, dims = 3, size = 1.):
+  def __init__(self, particles = 1, dims = 3, size = 1.):
     """
     Parameters
     -
@@ -16,22 +15,42 @@ class config_walker:
       Number of particles in the system
     dims: int
       Number of dimensions for the system
-    size: float or tuple
-      Size of the system in each direction
+    size: float
+      Size of the system in each dimension
     """
+    self.particles = particles
+    self.dims = dims
+    self.size = size
     self.coord_n = dims * particles
-    self.positions = self.rng.random(self.coord_n) * size
+    self.positions = self.rng.uniform(0, size, self.coord_n)
     self.max_step = 0.001 * size
-    self.id = self.next_id
-    self.next_id += 1
 
-  def step(self):
+  def step_destination(self):
+    """Try stepping to a new configuration, but don't do it yet. Used in Markov chain implementation"""
     step_axis = self.rng.integers(0, self.coord_n)
     step_len = self.rng.uniform(-self.max_step, self.max_step)
-    self.positions[step_axis] += step_len
+    step_vec = np.zeros_like(self.positions)
+    step_vec[step_axis] += step_len
+    return self.positions + step_vec
+  
+  def step_to(self, destination: np.ndarray):
+    """Move to a new configuration."""
+    self.positions = destination
+
+  def step(self):
+    dest = self.step_destination()
+    self.step_to(dest)
 
   def current_config(self):
-    return self.positions
+    """Returns a copy of the current configuration"""
+    return self.positions.copy()
   
   def __str__(self):
-    return f"Walker n. {self.id} at {self.current_config()}"
+    return f"Walker with N = {self.particles}, d = {self.dims}, L = {self.size} at {self.current_config()}"
+
+wa = ConfigWalker()
+for i in range(0, 25):
+  dest = wa.step()
+
+  
+
