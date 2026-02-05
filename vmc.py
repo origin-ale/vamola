cfa25e9dafb3c6528d92605a7a618e5ada869f62
Alessandro Cerati <alessandro.cerati@studio.unibo.it>
2026-02-05 16:59:02 +0100

    Want back to walker class, implemented Metropolis step

--- vmc.py
+++ vmc.py
@@ -1,33 +1,68 @@
 import numpy as np
 
-class ConfigSpace:
+class ConfigWalker:
   """
-  Configuration space for N particles in D dimensions, each of size 2L and divided into a number of subdivisions.
+  Random walker representing the configuration space position of a system of N particles in d dimensions of size L
   """
   rng_seed = 1
   rng = np.random.default_rng(rng_seed)
 
-  def __init__(self, particles: int, subdivisions: int, dim_size = 1., dims = 3):
-    self.spatial_dims = dims
-    self.dimension = particles * dims
-    self.size = dim_size
-    self.subdivisions = subdivisions
+  def __init__(self, particles = 1, dims = 3, scale = 1.):
+    """
+    Parameters
+    -
+    particles: int
+      Number of particles in the system
+    dims: int
+      Number of dimensions for the system
+    size: float
+      Size of the system in each dimension
+    """
+    self.particles = particles
+    self.dims = dims
+    self.scale = scale
+    self.coord_n = dims * particles
+    self.positions = self.rng.uniform(0, scale, self.coord_n)
+    self.max_step = 0.01 * scale
 
-  def step_from(self, initial: np.ndarray, max_step: int | None = None):
-    if initial.shape != (self.dimension,): raise ValueError("Initial position is wrong shape!")
-    if max_step == None: max_step = max(1,self.subdivisions//1e3)
-    step_axis = self.rng.integers(0, self.dimension)
-    step_len = self.rng.integers(-max_step, max_step)
-    step_vec = initial.copy()
-    if step_vec[step_axis] - step_len <= 0: 
-      step_vec[step_axis] = 0
-    elif step_vec[step_axis] + step_len >= self.subdivisions-1: 
-      step_vec[step_axis] = self.subdivisions-1
-    else: step_vec[step_axis] += step_len
-    return step_vec
+  def step_from(self):
+    """Try stepping to a new configuration, but don't do it yet. Used in Markov chain implementation"""
+    step_axis = self.rng.integers(0, self.coord_n)
+    step_len = self.rng.uniform(-self.max_step, self.max_step)
+    step_vec = np.zeros_like(self.positions)
+    step_vec[step_axis] += step_len
+    return self.positions + step_vec
+  
+  def move_to(self, destination: np.ndarray):
+    """Move to a new configuration."""
+    self.positions = destination
+
+  def step(self):
+    dest = self.step_from()
+    self.move_to(dest)
+
+  def metropolis_step(self, wf: function):
+    r = self.current_config()
+    rp = self.step_from()
+    p = (wf(rp)**2)/(wf(r)**2)
+    if p >= 1: self.move_to(rp)
+    elif self.rng.random() < p: self.move_to(rp)
+    else: print("Move rejected")
+
+  def current_config(self):
+    """Returns a copy of the current configuration"""
+    return self.positions.copy()
   
   def __str__(self):
-    particles = self.dimension//self.spatial_dims
-    return f"{self.dimension}-dimensional config space (N = {particles}, D = {self.spatial_dims});"+\
-          f" {self.subdivisions} positions per axis, axis length {self.size}"
-  
+    return f"Walker with N = {self.particles}, d = {self.dims}, L = {self.scale} at {self.current_config()}"
+
+def psi_t(r: np.ndarray):
+  r0 = np.array([0,0,0])
+  dist = np.linalg.norm(r - r0)
+  return np.exp(-1000000*dist**2)
+
+wa = ConfigWalker()
+wa.positions = np.array([0.01,0.01,0.01])
+for i in range(0,10000):
+  print(wa.current_config())
+  wa.metropolis_step(psi_t)
