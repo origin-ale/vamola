f547ace08bdfbec2d366c23796c45107f43c669c
Alessandro Cerati <alessandro.cerati@studio.unibo.it>
2026-02-05 15:44:13 +0100

    Replaced Walker class with ConfigSpace class

--- vmc.py
+++ vmc.py
@@ -1,56 +1,33 @@
 import numpy as np
 
-class ConfigWalker:
+class ConfigSpace:
   """
-  Random walker representing the configuration space position of a system of N particles in d dimensions of size L
+  Configuration space for N particles in D dimensions, each of size 2L and divided into a number of subdivisions.
   """
   rng_seed = 1
   rng = np.random.default_rng(rng_seed)
 
-  def __init__(self, particles = 1, dims = 3, size = 1.):
-    """
-    Parameters
-    -
-    particles: int
-      Number of particles in the system
-    dims: int
-      Number of dimensions for the system
-    size: float
-      Size of the system in each dimension
-    """
-    self.particles = particles
-    self.dims = dims
-    self.size = size
-    self.coord_n = dims * particles
-    self.positions = self.rng.uniform(0, size, self.coord_n)
-    self.max_step = 0.001 * size
+  def __init__(self, particles: int, subdivisions: int, dim_size = 1., dims = 3):
+    self.spatial_dims = dims
+    self.dimension = particles * dims
+    self.size = dim_size
+    self.subdivisions = subdivisions
 
-  def step_destination(self):
-    """Try stepping to a new configuration, but don't do it yet. Used in Markov chain implementation"""
-    step_axis = self.rng.integers(0, self.coord_n)
-    step_len = self.rng.uniform(-self.max_step, self.max_step)
-    step_vec = np.zeros_like(self.positions)
-    step_vec[step_axis] += step_len
-    return self.positions + step_vec
-  
-  def step_to(self, destination: np.ndarray):
-    """Move to a new configuration."""
-    self.positions = destination
-
-  def step(self):
-    dest = self.step_destination()
-    self.step_to(dest)
-
-  def current_config(self):
-    """Returns a copy of the current configuration"""
-    return self.positions.copy()
+  def step_from(self, initial: np.ndarray, max_step: int | None = None):
+    if initial.shape != (self.dimension,): raise ValueError("Initial position is wrong shape!")
+    if max_step == None: max_step = max(1,self.subdivisions//1e3)
+    step_axis = self.rng.integers(0, self.dimension)
+    step_len = self.rng.integers(-max_step, max_step)
+    step_vec = initial.copy()
+    if step_vec[step_axis] - step_len <= 0: 
+      step_vec[step_axis] = 0
+    elif step_vec[step_axis] + step_len >= self.subdivisions-1: 
+      step_vec[step_axis] = self.subdivisions-1
+    else: step_vec[step_axis] += step_len
+    return step_vec
   
   def __str__(self):
-    return f"Walker with N = {self.particles}, d = {self.dims}, L = {self.size} at {self.current_config()}"
-
-wa = ConfigWalker()
-for i in range(0, 25):
-  dest = wa.step()
-
+    particles = self.dimension//self.spatial_dims
+    return f"{self.dimension}-dimensional config space (N = {particles}, D = {self.spatial_dims});"+\
+          f" {self.subdivisions} positions per axis, axis length {self.size}"
   
-
