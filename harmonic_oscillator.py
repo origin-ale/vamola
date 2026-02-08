ce3ce2fdb563d15ec7d3a1ccef9f5b3b38be20c9
Alessandro Cerati <alessandro.cerati@studio.unibo.it>
2026-02-08 22:21:47 +0100

    Fix harmonic oscillator energies being 1-element arrays

--- harmonic_oscillator.py
+++ harmonic_oscillator.py
@@ -3,13 +3,16 @@ import numpy as np
 import vmc
 import output_utils as ou
 
-def e_l_alpha(x:float, alpha: float):
+def e_l_alpha(R:np.ndarray, alpha: float):
+  x = np.linalg.vector_norm(R)
   return alpha + x**2 * (0.5 - 2*(alpha**2))
 
-def psi_alpha(x:float, alpha:float):
+def psi_alpha(R:np.ndarray, alpha:float):
+  x = np.linalg.vector_norm(R)
   return np.exp(-alpha * (x**2))
 
-def logder(x:float, alpha:float):
+def logder(R:np.ndarray, alpha:float):
+  x = np.linalg.vector_norm(R)
   return -(x**2)
 
 if __name__=="__main__":
